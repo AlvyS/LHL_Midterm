28dc9f3194ddee164cc08aa5ca3172361ebe8fde
fayezosaadi <fayezosaadi@gmail.com>
2017-04-27 21:41:48 +0000

    cart_table_session_id

--- db/seeds/6cart.js
+++ db/seeds/6cart.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
 return knex('cart').del()
   .then(function () {
     return Promise.all([
-      knex('cart').insert({id: 1, item_id:1 , price:11 , quantity:1}),
+      knex('cart').insert({id: 1, item_id:'1' , price:'11' , quantity:'1', session_id:'100'}),
     ]);
   });
 };
