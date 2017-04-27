160dc699b5a2af9563000ba33ace40a48c43500a
fayezosaadi <fayezosaadi@gmail.com>
2017-04-27 01:20:47 +0000

    users seed file

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
-  return knex('users').del()
-    .then(function () {
-      return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
-      ]);
-    });
+ return knex('users').del()
+   .then(function () {
+     return Promise.all([
+       knex('users').insert({id: 1, first_name: 'Alice', last_name: 'Collin', phone_number:'222222'}),
+       knex('users').insert({id: 2, first_name: 'Sean', last_name: 'Park', phone_number:'32222222'}),
+       knex('users').insert({id: 3, first_name: 'Julie', last_name: 'Stevens', phone_number:'42222222'}),
+     ]);
+   });
 };
