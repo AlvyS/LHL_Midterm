aed4906605a32839988018824918c441e927ebef
fayezosaadi <fayezosaadi@gmail.com>
2017-04-27 05:51:43 +0000

    dummy_seeds phone_numbers confirmed

--- db/seeds/2users.js
+++ db/seeds/2users.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
  return knex('users').del()
    .then(function () {
      return Promise.all([
-       knex('users').insert({id: 1, first_name: 'Alice', last_name: 'Collin', phone_number:'222222'}),
-       knex('users').insert({id: 2, first_name: 'Sean', last_name: 'Park', phone_number:'32222222'}),
-       knex('users').insert({id: 3, first_name: 'Julie', last_name: 'Stevens', phone_number:'42222222'}),
+       knex('users').insert({id: 1, first_name: 'Alice', last_name: 'Collin', phone:'222222'}),
+       knex('users').insert({id: 2, first_name: 'Fayez', last_name: 'Saadi', phone:'+17782512517'}),
+       knex('users').insert({id: 3, first_name: 'Sean', last_name: 'Park', phone:'42222222'}),
      ]);
    });
 };
