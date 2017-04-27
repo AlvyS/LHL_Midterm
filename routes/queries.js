5badb8234b288af6801efe6b3afaf82d0eeced42
sindhupriya-madala <madala.sindhu@gmail.com>
2017-04-27 01:12:19 +0000

    in feature/initial_routes branch

---
+++ routes/queries.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const express = require('express');
+const router  = express.Router();
+
+let knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'labber',
+    password: 'labber',
+    database: 'midterm'
+  }
+});
+
+module.exports = {
+
+  //get home page with all menu list
+  //router.get("/", (req, res) => {
+  getRoute: (done) => {
+    knex
+      .select("*")
+      .from("users")
+      .then((results) => {
+        done(results);
+        console.log("from queries.json inside getAllUsers");
+        //res.render("index");
+    });
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,50 @@
 
 const express = require('express');
 const router  = express.Router();
+const util = require('./queries');
 
 module.exports = (knex) => {
 
+  //get home page with all menu list
   router.get("/", (req, res) => {
+
+    util.getRoute((users) => {
+      console.log("inside users.js ");
+      res.render("index");
+    });
+
+
+  /*router.get("/users", (req, res) => {
     knex
       .select("*")
       .from("users")
       .then((results) => {
-        res.json(results);
+        res.json({"get" : "hey i am from /users"});
     });
   });
 
+  router.get("/order", (req, res) => {
+    res.json({"get" : "hey i am from /order"});
+  });
+
+  router.get("/order/:id", (req, res) => {
+    const order_id = req.params.id;
+    res.json({"get" : `hey i am from /order/${order_id}`});
+  });
+
+  router.get("/order/:id/checkout", (req, res) => {
+    const order_id = req.params.id;
+    res.json({"get" : `hey i am from /order/${order_id}/checkout`});
+  });
+
+  router.post("/order", (req, res) => {
+    res.json({"get" : "hey i am from /order"});
+  });
+
+  router.post("/order/:id/checkout", (req, res) => {
+    res.json({"get" : "hey i am from /order/:id/checkout"});
+  });
+*/
+});
   return router;
 }
