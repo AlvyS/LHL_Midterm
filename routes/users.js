859b5091a88cc8c8c0a99eb7f9ca33f80f78b252
fayezosaadi <fayezosaadi@gmail.com>
2017-04-28 21:05:29 +0000

    finalizing the routes

--- routes/users.js
+++ routes/users.js
@@ -9,19 +9,15 @@ module.exports = (knex) => {
 
   //get home page with all menu list
   router.get("/", (req, res) => {
-
     queries.getItems(knex, (items) => {
-      console.log("inside users.js :",items);
-      //res.json( {"get" : items});
-      // var firstItem = items[0]
-      // res.render('index', firstItem);
-      // console.log(firstItem);
-
-      res.render('index',{allitems :items});
+      // console.log("inside users.js :",items);
+      var allItems = {allitems :items}
+      res.render('index', allItems);
     });
   });
 
-  //add item of particulas session into cart.
+  //add item of particular session into cart.
+  //flash message added to cart
   router.post("/cart/:session_id/:item_id/add", (req, res) => {
     const cart = {
       session_id : req.params.session_id,
@@ -30,18 +26,19 @@ module.exports = (knex) => {
       quantity : req.body.quantity
     }
     queries.addItemToCart(knex, cart, () => {
-      console.log("inside post req : cart/session/item");
-      res.json({"get" : "hey i am from cart/:session_id/:item_id"});
+      // console.log("inside post req : cart/session/item");
+      // res.json({"get" : "hey i am from cart/:session_id/:item_id"});
     });
   });
 
   //get particular session cart details.
   router.get("/cart/:session_id", (req, res) => {
-    // console.log(req.params.session_id);
     const session_id = req.params.session_id;
     queries.getSessionCart(knex,session_id, (item) => {
-      console.log("inside users.js :",item);
-      res.json({"get  cart details of pirticular user : " : item});
+      // console.log("inside users.js :",item);
+      // res.json({"get  cart details of pirticular user : " : item});
+      var allItems = {allitems :item}
+      res.render('cart', allItems);
     });
   });
 
@@ -54,8 +51,8 @@ module.exports = (knex) => {
       quantity : req.body.quantity
     }
     queries.updateCartItem(knex, cart, () => {
-      console.log("inside post req : cart/session/item");
-      res.json({"get" : "hey i am from cart/:session_id/:item_id"});
+      // console.log("inside post req : cart/session/item");
+      // res.json({"get" : "hey i am from cart/:session_id/:item_id"});
     });
   });
 
@@ -67,8 +64,8 @@ module.exports = (knex) => {
       quantity : req.body.quantity
     }
     queries.deleteCartItem(knex, cart, () => {
-      console.log("inside post req : cart/session/item");
-      res.json({"get" : "hey i am from cart/:session_id/:item_id"});
+      // console.log("inside post req : cart/session/item");
+      // res.json({"get" : "hey i am from cart/:session_id/:item_id"});
     });
   });
 
@@ -77,11 +74,14 @@ module.exports = (knex) => {
     const session_id = req.params.session_id;
     queries.getSessionCart(knex,session_id, (item) => {
       console.log("inside users.js :",item);
-      res.json({"get  cart details of pirticular user : " : `${item}`});
+      // res.json({"get  cart details of pirticular user : " : `${item}`});
+      var allItems = {allitems :item}
+      res.render('checkout', allItems);
     });
   });
 
-  //place order.
+  //place order
+  //flash_order
   router.post("/placeorder/:session_id", (req, res) => {
     const cart = {
       session_id : req.params.session_id,
@@ -91,82 +91,11 @@ module.exports = (knex) => {
       first_name : req.body.firstname,
       last_name : req.body.lastname,
       phone : req.body.phone,
-
     }
     queries.placeOrder(knex, cart, () => {
-      console.log("inside post req : cart/session/item");
-      res.json({"get" : "hey i am from cart/:session_id/:item_id"});
+      res.redirect('/');
     });
   });
-
-
- /* router.post("/order/:id", (req, res) => {
-    const order  = {
-      order_id : req.params.id,
-      item_id : req.body.item_id,
-      quantity : req.body.quantity,
-      price : req.body.price,
-      status : req.body.status
-    }
-    console.log(order);
-    queries.postOrderDetails(knex,order, () => {
-      console.log("inside users.js :");
-      res.json({"posted perticular order details" : "postOrderDetails"});
-    });
-  });
-
-  router.get("/users", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json({"get" : "hey i am from /users"});
-    });
-  });*/
-
-  //calls getAllOrders in queries.js.
-  /*router.get("/order", (req, res) => {
-
-    queries.getAllOrders(knex, (order) => {
-      console.log("inside users.js :",order);
-      res.json({"get" : `${order}`});
-    });
-  });*/
-
-  //to get order details of particular order.
-  /*router.get("/order/:id", (req, res) => {
-    const order_id = req.params.id;
-    queries.getOrderDetails(knex,order_id, (order) => {
-      console.log("inside users.js :",order);
-      res.json({"get perticular order id : " : `${order}`});
-    });
-  });*/
-
-  //get checkout details of a pirticular user.
-  /*router.get("/order/:id/checkout", (req, res) => {
-    const order_id = req.params.id;
-    queries.getOrderCheckout(knex,order_id, (order) => {
-      console.log("inside users.js :",order_id);
-      res.json({"get perticular order checkout : " : `${order}`});
-    });
-  });*/
-
-  //get individual item details from item table.
-  /*router.get("/items/:id", (req, res) => {
-    const item_id = req.params.id;
-    queries.getItemDetails(knex,item_id, (item) => {
-      console.log("inside users.js :",item);
-      res.json({"get perticular order checkout : " : `${item}`});
-    });
-  });*/
-
-
-
-
- /* router.post("/order/:id/checkout", (req, res) => {
-    res.json({"get" : "hey i am from /order/:id/checkout"});
-  });
-*/
 
   return router;
 }
