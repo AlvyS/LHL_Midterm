2c0238a2e36136490b559eb5a11e82a645e05548
AlvyS <AlvyShih@hotmail.com>
2017-04-29 03:11:13 +0000

    implented shopping cart

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,24 +2,39 @@ $(document).ready(function () {
 // //------------------------------------------------------------------------------------------------------------------------
 
 
-// //------------------- Open Modal popup when clicking on an item ------------------------------------
-  $('.item-container').on('click', '.item-photo', function(event) {
-    // console.log(event);
-    // console.log($(this));
-    // console.log(this); 
-    $(this).find('.modal').toggleClass('active');
-    // console.log($(this).find('.modal'));
+// // //------------------- Open Modal popup when clicking on cart ------------------------------------
+//   $('.item-container').on('click', '.item-photo', function(event) {
+//     // console.log(event);
+//     // console.log($(this));
+//     // console.log(this); 
+//     $(this).find('.modal').toggleClass('active');
+//     // console.log($(this).find('.modal'));
+//   });
+
+// // //------------------- Close cart when clicking "X" -------------------------------------------
+//   $('.modal-close').on('click', function(event) {
+//     $(this).find('.modal').toggleClass('active');
+//   });
+
+
+// //------------------- Open Modal popup when clicking on cart ------------------------------------
+  $('#cart-container').on('click', function(event) {
+    console.log("i clicked!")
+    $('.modal-cart').toggleClass('active');
+    console.log($('.modal-cart'))
   });
 
-// //------------------- Close Modal Popup when clicking "X" -------------------------------------------
+// //------------------- Close cart when clicking "X" -------------------------------------------
   $('.modal-close').on('click', function(event) {
-    $(this).find('.modal').toggleClass('active');
+    $('.modal-cart').toggleClass('active');
   });
 
 
 // //--------------
-
-
+  $('.checkout-row').on('click', function(event) {
+    console.log("i moused!!")
+    $('.checkout-row').toggleClass('active');
+  });
 
 
 
