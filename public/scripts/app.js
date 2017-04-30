a5900113f75425aa4781b130518b869f556153d1
Sean Park <stpnpark@gmail.com>
2017-04-30 14:37:03 -0700

    app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -57,26 +57,67 @@ $('.cd-item-info').on('click', '.info-container', function(event) {
 
 
 function getCartItems() {
-  var $infoModal = $('#cart-container');
   $('.cart-button').on('click', function(){
       $.ajax({
-          type: "GET",
-          url: '/cart',
+          url: '/cartpopup',
+          method: "GET",
           dataType: 'json',
-          error: function(items){
-              fakeResponse = {"id":4,"menu_category_id":446,"name":"kunzereichert","description":"Dolores impedit ut doloribus et a et aut.","price":"999.99","created_at":"2015-04-10 05:55:23","updated_at":"2015-04-10 05:55:23"}
-;
-              var htmlData = '<ul><li>';
-              htmlData += fakeResponse.name;
-              htmlData += '</li></ul>';
-              for(let item of items){
-                console.log(`dsaflksafljdlsa`);
-                $('.modal-cart-items')[0].innerHTML = item;
-              }
-              infoModal.modal();
+          success: function(items){
+            let htmlData ='<form>';
+            let total = 0;
+            console.log('sfdsafdsafdsafdsafds',items);
+            if(items){
+              items.forEach( (item) => {
+                var loopData = 
+                `<div class="row">
+                    <div class="column small-12 medium-3">
+                      <h3> ${item.name}</h3>
+                    </div>
+                    <div class="column small-12 medium-5">
+                      <h3 name = "price"> ${item.price} </h3>
+                    </div> 
+                    <div class="input-group column small-12 medium-4">
+                      <h3> ${item.quantity} </h3>
+                      <input class="input-group-field" name = "quantity" type="number" for="btn btn-default">
+                    </div>
+                    <div class="row">
+                      <button class="small-12 medium-6" type="submit" formmethod="POST" formaction="cart/${item.item_id}/update" >Edit</button>
+                      <button class="small-12 medium-6" type="submit" formmethod="POST" formaction="cart/${item.item_id}/delete" >Remove</button>
+                    </div>                    
+                  </div>
+                  `;
+                  htmlData += loopData;
+                  total += item.price*item.quantity;
+              });
+                let totalData = `<div class="row">
+              <div class="column small-12 medium-3">
+
+              </div>
+              <div class="column small-12 medium-9">
+                <h3 name="total">Total: ${total} </h3>
+              </div>
+            </div>
+          </form>
+          <form> 
+            <div class="row">
+              <div class="column small-12 medium-3">
+                <!-- <a class="button primary" type="submit">Add to Cart</a> -->
+                <!-- <input type="submit" value="Tweet"> -->
+                <button type="submit" class="btn btn-default" formmethod="GET" formaction="/checkout" >Checkout</button>
+              </div>
+              <div class="column small-12 medium-9">
+                <a class="button warning" href="/">Cancel</a>
+              </div>
+            </div>
+          </form>
+          `;
+
+              htmlData += totalData;
+          }
+              $('.modal-cart-items')[0].innerHTML = htmlData;
           }
       });
   });
 }
-
+getCartItems();
 });
