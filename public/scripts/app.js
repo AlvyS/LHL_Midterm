2a06b6f430d37badd9c7113d61c6ea67b028fa5b
Sean Park <stpnpark@gmail.com>
2017-04-29 17:35:52 -0700

    index css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,22 +3,42 @@ $(document).ready(function () {
 
 
 // //------------------- Open Modal popup when clicking on cart ------------------------------------
-  $('#cart-container').on('click', function(event) {
-    console.log("i clicked!")
-    $('.modal-cart').toggleClass('active');
-    console.log($('.modal-cart'))
-  });
+$('#cart-container').on('click', function(event) {
+  console.log("i clicked!")
+  $('.modal-cart').toggleClass('active');
+  console.log($('.modal-cart'))
+});
 
 // //------------------- Close cart when clicking "X" -------------------------------------------
-  $('.modal-close').on('click', function(event) {
-    $('.modal-cart').toggleClass('active');
-  });
+$('.modal-close').on('click', function(event) {
+  $('.modal-cart').toggleClass('active');
+});
 
 
 // //--------------
-  $('.checkout-row').on('click', function(event) {
-    $('.checkout-row').toggleClass('active');
-  });
+$('.checkout-row').on('click', function(event) {
+  $('.checkout-row').toggleClass('active');
+});
+
+
+// //------item desc event delegation 
+$('.cd-item-info').on('click', '.info-container', function(event) {
+    $(this).find('.item-desc').slideToggle();;
+});
+
+// $('.product-name').on('click', function(event) {
+//   $('.item-desc').slideToggle();
+// });
+
+// const $desc = $('.item-desc');
+// $('.cd-single-item').on('click', () => {
+//   if($desc.is(':hidden')){
+//     $(this).slideDown(650, () => {
+//       $(this).trigger('reset');
+//     });
+//   }
+// });
+
 
 
 
